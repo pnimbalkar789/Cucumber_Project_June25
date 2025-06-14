9172b0c10cde411bbf0201735410a60af86dead2
shital <shitalnimbalkar92@gmail.com>
2025-06-14 20:35:09 +0530

    first commit

---
+++ target/cucumber-report/report.js
@@ -0,0 +1,360 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/java/features/Login.feature");
+formatter.feature({
+  "name": "Login Feature",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I have launched the simplilearn application",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_have_launched_the_simplilearn_application()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click on the Login Link",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_click_on_the_Login_Link()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Login Success",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "I enter the Username",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_enter_the_Username()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I enter the Password",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_enter_the_Password()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click on the Login Button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_click_on_the_Login_Button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I should be able to see the home page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_should_be_able_to_see_the_home_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I have launched the simplilearn application",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_have_launched_the_simplilearn_application()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click on the Login Link",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_click_on_the_Login_Link()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Login Failure Scenario using test data as parameter",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "I enter the Username \"abc@xyz.com\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_enter_the_Username(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I enter the Password \"Abc@12345\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_enter_the_Password(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click on the Login button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_click_on_the_Login_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I should be able to see the error message \"The email address or password you have entered is invalid\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_should_be_able_to_see_the_error_message(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.scenarioOutline({
+  "name": "Login Failure Scenario using test data as Examples",
+  "description": "",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "name": "I enter the Username \"\u003cUserName\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "I enter the Password \"\u003cPassword\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "I click on the Login button",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "I should be able to see the error message \"\u003cError\u003e\"",
+  "keyword": "Then "
+});
+formatter.examples({
+  "name": "",
+  "description": "",
+  "keyword": "Examples",
+  "rows": [
+    {
+      "cells": [
+        "UserName",
+        "Password",
+        "Error"
+      ]
+    },
+    {
+      "cells": [
+        "abc@xyz.com",
+        "Abc@1234",
+        "The email address or password you have entered is invalid"
+      ]
+    },
+    {
+      "cells": [
+        "pqr@xyz.com",
+        "Acvb@1234",
+        "The email address or password you have entered is invalid"
+      ]
+    }
+  ]
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I have launched the simplilearn application",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_have_launched_the_simplilearn_application()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click on the Login Link",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_click_on_the_Login_Link()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Login Failure Scenario using test data as Examples",
+  "description": "",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "name": "I enter the Username \"abc@xyz.com\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_enter_the_Username(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I enter the Password \"Abc@1234\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_enter_the_Password(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click on the Login button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_click_on_the_Login_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I should be able to see the error message \"The email address or password you have entered is invalid\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_should_be_able_to_see_the_error_message(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I have launched the simplilearn application",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_have_launched_the_simplilearn_application()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click on the Login Link",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_click_on_the_Login_Link()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Login Failure Scenario using test data as Examples",
+  "description": "",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "name": "I enter the Username \"pqr@xyz.com\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_enter_the_Username(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I enter the Password \"Acvb@1234\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_enter_the_Password(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click on the Login button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_click_on_the_Login_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I should be able to see the error message \"The email address or password you have entered is invalid\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefs.Login_Stepdefs.i_should_be_able_to_see_the_error_message(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+});
